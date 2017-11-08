3ddd7fc1c5db2b8d729f2cc05ac7106c1dd89341
rvexiau <romain.vexiau@u-psud.fr>
2017-11-08 17:25:03 +0100

    final initial commit

---
+++ compile.sh
@@ -0,0 +1,70 @@
+cd CIPSI_sources
+
+cd bd
+make 
+cd ..
+
+cd bdsym
+make 
+cd ..
+
+cd cip
+make
+cd ..
+
+cd cipsym
+make
+cd ..
+
+cd ciro
+make
+cd ..
+
+cd cval
+make
+cd ..
+
+cd fock
+make 
+cd ..
+
+cd ijkl
+make 
+cd ..
+
+cd moy
+make
+cd ..
+
+cd moysym
+make
+cd ..
+
+cd pshf
+make 
+cd ..
+
+cd rcut
+make 
+cd ..
+
+cd rpol
+make
+cd ..
+
+cd spin
+make 
+cd ..
+
+cd spinsym
+make 
+cd ..
+
+cd hdiab
+make
+cd ..
+
+cd ..
+cd Autocip_13
+make 
+cd ..
